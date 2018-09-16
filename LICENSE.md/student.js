19b6919235c57d877ae5858701548e77b1e09a3e
Jonathan Peters <42306733+JonathanPetersCode@users.noreply.github.com>
2018-09-16 08:13:29 -0400

    Update student.js

--- LICENSE.md/student.js
+++ LICENSE.md/student.js
@@ -1,16 +1,51 @@
-var message = '';
-var student;
-
-function print(message) {
-  var outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = message;
-}
-
-for (var i = 0; i < students.length; i += 1) {
-  student = students[i];
-  message += '<h2>Student: ' + student.name + '</h2>';
-  message += '<p>Track: ' + student.track + '</p>';
-  message += '<p>Points: ' + student.points + '</p>';
-  message += '<p>Achievements: ' + student.achievements + '</p>';
-}
-print(message);
+var students = [
+  {
+    name: "Mark" , 
+    track: "IOS Development", 
+    achievements: "43", 
+    points: "1231"
+  },
+  
+  {
+    name: "Ben",
+    track: "Back End Development", 
+    achievements: "84" ,
+    points: "3213"
+  },
+  
+  {
+    name: "Dave",
+    track: "Front End Development" , 
+    achievements: "35" ,
+    points: "5124"
+  },
+  
+  {
+    name: "Sarah" ,
+    track: "Android Development",
+    achievements: "72" , 
+    points: "2112"
+  },
+  
+  {
+    name: "Jess" ,
+    track: "Computer Science", 
+    achievements: "90" ,
+    points: "7419"
+  },
+  
+  {
+    name: "Sara" ,
+    track: "Web Design" ,
+    achievements: "51" , 
+    points: "8412"
+  },
+  
+  {
+    name: "Malik" ,
+    track: "Back End Development", 
+    achievements: "32" ,
+    points: "9471"
+  
+  }
+];
